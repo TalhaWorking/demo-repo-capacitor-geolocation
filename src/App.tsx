e958fb1d75c5ac245d52fb739a201b1898edab45
talha-bin-irshad <talhaarshaad5@gmail.com>
2024-01-20 17:05:29 +0500

    capacitor geolocation not working - this is a demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,52 @@
-import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
+import { useState } from "react";
+
+import { Geolocation } from "@capacitor/geolocation";
+import {
+  IonApp,
+  IonButton,
+  setupIonicReact,
+} from "@ionic/react";
 
 /* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
+import "@ionic/react/css/core.css";
 
 /* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/typography.css";
 
 /* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/display.css";
 
 /* Theme variables */
-import './theme/variables.css';
+import "./theme/variables.css";
 
 setupIonicReact();
 
-const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonRouterOutlet>
-        <Route exact path="/home">
-          <Home />
-        </Route>
-        <Route exact path="/">
-          <Redirect to="/home" />
-        </Route>
-      </IonRouterOutlet>
-    </IonReactRouter>
-  </IonApp>
-);
+const App: React.FC = () => {
+  const [loader, setLoader] = useState(false)
+  const testingCheckPermissions = async (): Promise<void> => {
+    try {
+      setLoader(true)
+      const checkPermission = await Geolocation.getCurrentPosition();
+      console.log({ checkPermission });
+      setLoader(false)
+    } catch (error) {
+      console.log(error);
+      setLoader(false) 
+    }
+  };
+  
+  return (
+    <IonApp>
+      <IonButton onClick={() => {void testingCheckPermissions()}} disabled={loader}>click</IonButton>
+    </IonApp>
+  );
+};
 
 export default App;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,13 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonContent, IonHeader, IonPage, IonText, IonTitle, IonToolbar, useIonPopover } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
 const Home: React.FC = () => {
+  const [openPopover] = useIonPopover(<>
+  <IonContent className='ion-padding'>
+    <IonText>Testing things...</IonText>
+  </IonContent>
+  </>)
   return (
     <IonPage>
       <IonHeader>
@@ -14,8 +19,14 @@ const Home: React.FC = () => {
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Blank</IonTitle>
+            
           </IonToolbar>
         </IonHeader>
+        <IonButton onClick={(_event: unknown) => {
+              openPopover({
+                event: _event as Event
+              })
+            }} size="large">Open</IonButton>
         <ExploreContainer />
       </IonContent>
     </IonPage>
